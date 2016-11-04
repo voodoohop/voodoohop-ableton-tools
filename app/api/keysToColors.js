4b3323b77805288c0efeaaeec3e57330632fb038
Thomas Haferlach <thomash@MacBooks-MacBook-Pro.local>
2016-11-04 12:56:32 -0700

    update

--- app/api/keysToColors.js
+++ app/api/keysToColors.js
@@ -18,36 +18,107 @@
 
 
 // const col=col3.map((c,i) => col3[(i*7)%col3.length]);
-// ;
-const col =["#8900ff","#0004ff","#637282","#00fbfb","#00ff91","#539933","#0ccc00","#83ff00","#fbfb00","#ff7c00","#ff000a","#f700fe"];
+// // ;
 
-//  console.log("colorwheel",col);
+// export const abletonCols = {
+// 	white: "#ffffff",
+// 	yellow: "#ffee9f",
+// 	yellow_1: "#dbc300",
+// 	yellow_2: "#ffee9f",
+// 	orange: "#f66c03",
+// 	red: "#ff0505",
+// 	dark_gray: "#3c3c3c",
+// 	green_1: "#89b47d",
+// 	green_2: "#87ff67",
+// 	blue_1: "#8bc5ff",
+// 	blue_2: "#10a4ee",
+// 	blue_3: "#0303ff",
+// 	purple_1: "#cdbbe4",
+// 	purple_2: "#d86ce4",
+// 	purple_3: "#ff39d4",
+// 	brown_1: '#965735',
+// 	brown_2: "#ffa374",
+// 	brown_3: "#ffa374",
+// 	fuschia_1: "#d2e498",
+// 	fuschia_2: "#7ac634",
+// 	pink: "#ff94a6"
+// };
 
-var colors=
+import tinycolor from 'tinycolor2';
+
+// const convertFormat = c => c.mapKeys(k => k.toUpperCase()).filter((v, k) => k !== "A");
+// const colsPre = Immutable.fromJS(abletonCols).toSet().map(col => tinycolor(col).toRgb()).map(c => Immutable.fromJS(c)).map(convertFormat); //;["#8900ff", "#0004ff", "#637282", "#00fbfb", "#00ff91", "#539933", "#0ccc00", "#83ff00", "#fbfb00", "#ff7c00", "#ff000a", "#f700fe"];
+
+
+// console.log("cols", colsPre);
+
+// import husl from "husl";
+
+// import colorDiff from "color-diff";
+
+// // col.reduce((remaining,color) => colorDiff.closest(color, remaining));
+
+// const startCol = colsPre.toArray()[Math.floor(Math.random() * colsPre.size)];
+
+
+// console.log("startCol", startCol);
+
+// // const colsByFurthest = Immutable.List();
+// const findNextCol = (cols, startCol, colsByFurthest) => {
+// 	// console.log(cols.toJS(), startCol);
+
+// 	const nextCol = colorDiff.closest(startCol.toJS(), cols.toJS());
+// 	if (!nextCol) {
+// 		console.log("colSequence", colsByFurthest);
+// 		return colsByFurthest;
+// 	}
+
+// 	console.log("nextCol", nextCol);
+// 	const newCols = cols.remove(Immutable.fromJS(nextCol));;
+// 	return findNextCol(newCols, Immutable.fromJS(nextCol), colsByFurthest.push(Immutable.fromJS(nextCol)));
+
+// };
+
+// const colsDifferent = findNextCol(colsPre.remove(startCol), startCol, Immutable.List()).take(12)
+// 	.map(c => "#" + tinycolor(c.mapKeys(k => k.toLowerCase()).toJS()).toHex()).toList();
+
+
+// const col = colsDifferent//Immutable.Range(0, 12 * 5, 5).map(i => colsDifferent.get(i % 12))
+// 	.toArray();
+
+const colDark = ['#bfba69', '#a6be00', '#7ac634', '#3dc300', '#00bfaf', '#10a4ee', '#5480e4', '#886ce4', '#a34bad', '#b73d69', '#965735', '#f66c03']
+	.map((col, i, cols) => cols[(i + 7) % cols.length]);
+const colLighter = ['#bffb00', '#87ff67', '#1aff2f', '#25ffa8', '#5cffe8', '#19e9ff', '#8bc5ff', '#92a7ff', '#b88dff', '#d86ce4', '#ff39d4', '#ffa529']
+	.map((col, i, cols) => cols[(i + 7) % cols.length]);
+const col = colLighter;
+console.log('colorwheel', col);
+import Immutable from 'immutable';
+
+let colors =
 	{
-		"c":col[0], "am":col[0],
-		"g":col[1], "em":col[1],
-		"d":col[2], "bm":col[2],
-		"a":col[3], "gbm":col[3],"f#m":col[3],
-		"e":col[4], "dbm":col[4],"c#m":col[4],
-		"b":col[5], "abm":col[5],"g#m":col[5],
-		"gb":col[6], "f#":col[6], "d#m":col[6], "ebm":col[6],
-		"db":col[7], "c#":col[7], "bbm":col[7], "a#m":col[7],
-		"ab":col[8], "g#": col[8], "fm":col[8],
-		"eb":col[9], "d#":col[9],"cm":col[9],
-		"bb":col[10], "a#":col[10], "gm":col[10],
-		"f":col[11],"dm":col[11]
+		'c': col[0], 'am': col[0],
+		g: col[1], 'em': col[1],
+		'd': col[2], bm: col[2],
+		a: col[3], gbm: col[3], 'f#m': col[3],
+		e: col[4], 'dbm': col[4], 'c#m': col[4],
+		b: col[5], 'abm': col[5], 'g#m': col[5],
+		'gb': col[6], 'f#': col[6], 'd#m': col[6], ebm: col[6],
+		db: col[7], 'c#': col[7], 'bbm': col[7], 'a#m': col[7],
+		ab: col[8], 'g#': col[8], 'fm': col[8],
+		'eb': col[9], 'd#': col[9], 'cm': col[9],
+		'bb': col[10], 'a#': col[10], gm: col[10],
+		f: col[11], 'dm': col[11]
 	}
-;
+	;
 
-import tinycolor from  "tinycolor2";
-console.log("colors",colors);
+
+console.log('colors', colors);
 export default function getKeyColor(keyString) {
-	// console.log("getting col for",keyString);	
+	// console.log("getting col for",keyString);
 	if (keyString === undefined)
-		return "#aaaaaa";
+		return '#aaaaaa';
 	if (keyString === null) {
 		return keyString;
 	}
-	return tinycolor(colors[keyString.toLowerCase().trim()] || "#aaaaaa")/*.lighten(20)*/.toHexString();
+	return tinycolor(colors[keyString.toLowerCase().trim()] || '#aaaaaa')/* .lighten(20)*/.toHexString();
 }
--- app/api/oscMetadataServer.js
+++ app/api/oscMetadataServer.js
@@ -1,45 +1,47 @@
-import {oscInputStream, oscOutput} from "../utils/oscInOut";
+import { oscInputStream, oscOutput } from "../utils/oscInOut";
 
-import {metadataStore} from "../store";
+import { getPathPromise } from "../store/metadataStore";
 
-import {Map,fromJS} from "immutable";
+import { Map, fromJS } from "immutable";
 
 import actionStream from "./actionSubject";
 
-import {from} from "most";
+import { from } from "most";
 
 import log from "../utils/streamLog";
 
 const loadingMetadata = oscInputStream
     // .tap(([cmd,data]) => console.log("cmd,data",cmd,data))
     .filter(([cmd, value]) => cmd == "get_metadata")
-    .map(([_,path]) => path)
+    .map(([_, path]) => path)
     // .tap(log("getMetadataPath"))
-    .flatMap(
-        path => metadataStore.map(metadata => Map({path, metadata:metadata.getIn([path,"id3Metadata"])})).skipImmRepeats()
-    )
+    // .flatMap(
+    //     path => metadataStore.map(metadata => Map({path, metadata:metadata.getIn([path,"id3Metadata"])})).skipImmRepeats()
+    // )
     .tap(log("getMetadataRequested"))
     .multicast();
 
 
-const needToLoad = loadingMetadata.filter(d => !d.get("metadata")).map((d) => Map({action: "loadMetadata", path: d.get("path")}))
+// const needToLoad = loadingMetadata.filter(d => !d.get("metadata")).map((d) => Map({action: "loadMetadata", path: d.get("path")}))
 
-actionStream.plug(needToLoad);
+// actionStream.plug(needToLoad);
 
 oscOutput.plug(loadingMetadata
+    .map(path => getPathPromise(path))
+    .await()
     // .map(d => d.get("metadata"))
     .filter(d => d.get("metadata"))
-    .flatMap(d => from(d.get("metadata").map((val,key)=> Map({trackId: "got_metadata", args:fromJS([d.get("path"),key,val])})).toArray()))
+    .flatMap(d => from(d.get("metadata").map((val, key) => Map({ trackId: "got_metadata", args: fromJS([d.get("path"), key, val]) })).toArray()))
     .tap(log("sendingBack"))
 );
-    
+
 
 
     // .combine(
     //     (path,metadata) => metadata.contains(path) ? 
     //         most.of(metadata.get(path))
     //         :
-            
+
     //         , metadataStore)
-    
+
     // ;
