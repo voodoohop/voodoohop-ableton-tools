f7cd5ce079f27eddbe0f83dbf56e68b0d374f862
Thomas Haferlach <thomash@MacBooks-MacBook-Pro.local>
2017-01-21 00:41:04 +0900

    mini update for splash screen

--- app/splashScreen.js
+++ app/splashScreen.js
@@ -31,8 +31,8 @@ export default component2(({foundMasterPlugin, foundTrackPlugin}) =>
   <div>
     <div className="ui inverted text container center aligned">
       <h2 className="ui header inverted">Voodoohop Live Tools</h2>
+      {getDraggableVoodoo({ message: "Drag to tracks you want to monitor", amxdPath: "Voodoohop Track Sender.amxd", disabled: foundTrackPlugin })}
       {getDraggableVoodoo({ message: "Drag to your master track", amxdPath: "Voodoohop Live Tools Master.amxd", disabled: foundMasterPlugin })}
-      {getDraggableVoodoo({ message: "Drag to a track you want to monitor", amxdPath: "Voodoohop Track Sender.amxd", disabled: foundTrackPlugin })}
     </div>
     <div className="ui icon inverted warning message ">
       <img className="ui mini image" src="../images/live_logo.png" />
